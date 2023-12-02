eba8795709977ebec1d83201c0d4ea479737ea24
Brian Whisler <brian.whisler@gmail.com>
2023-12-02 08:30:18 -0700

    add button on home page for highscores

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,11 +6,20 @@ export default function Home() {
     <Box
       sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}
     >
-      <Typography sx={{ my: '10em' }} variant="body1">Welcome to the Roost!</Typography>
-      <Button component={ReactRouter} to="/play" variant='outlined'>
+      <Typography sx={{ my: '10em' }} variant="body1">
+        Welcome to the Roost!
+      </Typography>
+      <Button
+        component={ReactRouter}
+        to="/play"
+        variant="outlined"
+        sx={{ m: 1 }}
+      >
         Play Game
+      </Button>
+      <Button component={ReactRouter} to="/highscores" variant="outlined">
+        High Scores
       </Button>
     </Box>
   );
 }
-
