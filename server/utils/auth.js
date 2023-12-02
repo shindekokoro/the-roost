28a2db09c69bc428c93120c26424cfecffbf5548
jeremysr <jeremysr@protonmail.com>
2023-12-02 13:42:31 -0700

    added dotenv to hide server side token secret

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,7 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-//TODO: hide this/change this
-const secret = 'mysecretssshhhhhhh';
+const secret = process.env.JWT_SECRET;
 const expiration = '2h';
 
 module.exports = {
