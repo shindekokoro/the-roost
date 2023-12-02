fd22bf99cef6f0f15f11f9dfda4f0ec1108a8c79
Brian Whisler <brian.whisler@gmail.com>
2023-12-02 06:27:22 -0700

    add play button to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,15 @@
-import { Typography } from '@mui/material';
+import { Link as ReactRouter } from 'react-router-dom';
+import { Box, Button, Typography } from '@mui/material';
 
 export default function Home() {
-  return <Typography variant="body1">Welcome to the Roost!</Typography>;
+  return (
+    <Box
+      sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}
+    >
+      <Typography sx={{ my: '10em' }} variant="body1">Welcome to the Roost!</Typography>
+      <Button component={ReactRouter} to="/play" variant='outlined'>
+        Play Game
+      </Button>
+    </Box>
+  );
 }
