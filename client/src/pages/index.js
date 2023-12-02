271014ed8bc16570309c226a99d4664c8818eb26
Brian Whisler <brian.whisler@gmail.com>
2023-12-02 08:30:33 -0700

    update exports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,9 @@
 import Home from './Home';
+import Score from './HighScores';
 import Error from './ErrorPage';
 import Login from './Login';
 import Logout from './Logout';
 import Profile from './Profile';
 import Play from './Play';
 
-export { Error, Home, Login, Logout, Profile, Play };
+export { Error, Home, Score, Login, Logout, Profile, Play };
