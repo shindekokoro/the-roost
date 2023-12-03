eb6f5a40f9eb9d770d63ff0511ba4b835cccbf59
Brian Whisler <brian.whisler@gmail.com>
2023-12-03 13:54:46 -0700

    move event functions to utils

---
+++ client/src/utils/newEvent.js
@@ -0,0 +1,35 @@
+import getEvent from './getEvent';
+
+/**
+ * Get a new event and set it in local storage and update state - this will cause the component to re-render
+ * @returns {void}
+ */
+const newEvent = (disableButtonsRef, data) => {
+  // reset the disable buttons ref so buttons will be enabled
+  disableButtonsRef.current = false;
+  // get a random event
+  let event = getEvent(data);
+  let newEnemyHP;
+  if (event.__typename === 'Combat') {
+    newEnemyHP = event.constitution * 10;
+  } else {
+    newEnemyHP = null;
+  }
+  let newCharacterHP = characterHP;
+  if (!newCharacterHP) {
+    newCharacterHP = data.currentPlayer[0].constitution * 10;
+  }
+  // set the event context in local storage)
+  setEventContext({
+    characterHP: newCharacterHP,
+    enemyHP: newEnemyHP,
+    currentEvent: event
+  });
+
+  // update state
+  setCharacterHP(characterHP);
+  setEnemyHP(newEnemyHP);
+  setCurrentEvent(event);
+};
+
+export default newEvent;
