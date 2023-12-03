64b72dd28b37b72c173ad36aba9246f59bf89d9c
Brian Whisler <brian.whisler@gmail.com>
2023-12-02 19:06:33 -0700

    update enemy status

--- client/src/components/Enemy.jsx
+++ client/src/components/Enemy.jsx
@@ -1,16 +1,38 @@
-import { Box, Typography } from '@mui/material';
-import { GiHealthPotion } from 'react-icons/gi';
+import { Box, Card, CardContent, Typography } from '@mui/material';
+import { GiHealthPotion, GiWisdom, GiOpenTreasureChest } from 'react-icons/gi';
+import { AiOutlineNumber } from 'react-icons/ai';
+import { FaHandFist, FaShieldHalved } from 'react-icons/fa6';
 
 export default function Enemy({ enemyData, hp }) {
   return (
-    <Box>
-      <Typography variant="h5">{enemyData.name}</Typography>
-      <Typography variant="body2">
-        HP: {hp} / {enemyData.constitution * 10} <GiHealthPotion /><br />
-        LVL: {enemyData.level}<br />
-        STR: {enemyData.strength}<br />
-        DEF: {enemyData.defense}<br />
-        CON: {enemyData.constitution}<br />
+    <Box
+      sx={{
+        textAlign: 'end',
+        background: '#05050550',
+        padding: 1,
+        borderRadius: '10%'
+      }}
+    >
+      <Typography variant="h6">{enemyData.name}</Typography>
+      <Typography variant="caption">
+        <Typography>
+          {hp} / {enemyData.constitution * 10} <GiHealthPotion />
+        </Typography>
+        <Typography>
+          {enemyData.level} <AiOutlineNumber />
+        </Typography>
+        <Typography>
+          {enemyData.strength} <FaHandFist />
+        </Typography>
+        <Typography>
+          {enemyData.defense} <FaShieldHalved />
+        </Typography>
+        <Typography>
+          {enemyData.constitution} <GiWisdom />
+        </Typography>
+        <Typography>
+          {enemyData.inventory.length} <GiOpenTreasureChest />
+        </Typography>
       </Typography>
     </Box>
   );
