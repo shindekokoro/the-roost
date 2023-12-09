c8e5adfb0ada80a3e917168fde35ded90ec216da
Brian Whisler <brian.whisler@gmail.com>
2023-12-09 05:41:25 -0700

    update rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,16 +1,26 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    'react/prop-types': 'disable',
+  env: {
+    browser: true,
+    es2020: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react/prop-types": 0,
+    quotes: ["error", "single", { allowTemplateLiterals: true }],
+    "comma-dangle": ["error", "never"],
+    semi: ["error", "always"],
+    indent: ["error", 2],
+  },
+};
